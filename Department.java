ec8f7163299c26dc8cbfeea3e83721671e35c9a6
01fe22bca057 <samuelkotikela14@gmail.com>
2024-02-10 11:21:19 +0530

    Details of employee

---
+++ Department.java
@@ -0,0 +1,18 @@
+
+
+class Department {
+     int deptId;
+     String deptName;
+
+    public Department(int deptId, String deptName) {
+        this.deptId = deptId;
+        this.deptName = deptName;
+    }
+
+    public int getDeptId() {
+        return deptId;
+    }
+    public String getDeptName() {
+        return deptName;
+    }
+}
